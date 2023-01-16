18825e1a1c24665e3f4879df78e039b613834e6a
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2023-01-16 09:11:11 +0300

    Add files via upload

---
+++ FrontEnd/src/Post.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from "react";
+import './Post.css'
+
+
+const BASE_URL = 'http://localhost:8000'
+
+
+function Post({post}){
+    const [imageUrl, setImageUrl] = useState('')
+
+    useEffect(()=>{
+        setImageUrl(BASE_URL + post.image_url)
+    }, [])
+
+const handleDelete = (event)=>{
+    event?.preventDefault()
+
+
+const requestOptions = {
+    method: 'DELETE'
+  }
+
+  fetch(BASE_URL + '/post/delete?id=' + post.id, requestOptions)
+    .then(response =>{
+        if (response.ok){
+            window.location.reload()
+        }
+        throw response
+    })
+}
+
+    return (
+        <div className='post'>
+        <img className='post_image' src={post.image_url}/>
+        <div className='post_content'>
+          <div className='post_title'>{post.title}</div>
+          <div className='post_creator'>by {post.creator}</div>
+          <div className='post_text'>{post.content}</div>
+          <div className='post_delete'>
+            <button onClick={handleDelete}>Delete post</button>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Post
